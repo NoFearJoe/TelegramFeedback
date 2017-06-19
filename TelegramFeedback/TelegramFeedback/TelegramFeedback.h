ecac6c29a402b58401bc9e09e862f532e3803b31
IlyaKharabet <ilyakharabet@gmail.com>
2017-06-19 17:17:49 +0300

    Create project

---
+++ TelegramFeedback/TelegramFeedback/TelegramFeedback.h
@@ -0,0 +1,19 @@
+//
+//  TelegramFeedback.h
+//  TelegramFeedback
+//
+//  Created by Ilya Kharabet on 19.06.17.
+//  Copyright © 2017 IK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TelegramFeedback.
+FOUNDATION_EXPORT double TelegramFeedbackVersionNumber;
+
+//! Project version string for TelegramFeedback.
+FOUNDATION_EXPORT const unsigned char TelegramFeedbackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TelegramFeedback/PublicHeader.h>
+
+
